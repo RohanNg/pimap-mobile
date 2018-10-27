6d9ee7babb8dde7c336edf6bb22d6ef0799e813d
Hai Dang Nguyen <hai.dang.nguyen@zalando.fi>
2018-10-27 17:43:19 +0300

    Add visibility input in Create Activity page

--- src/screens/MyActivities.tsx
+++ src/screens/MyActivities.tsx
@@ -12,16 +12,36 @@ import {
 import * as firebase from 'firebase'
 import { tabBarIcon } from '../components/navigation/tabBarIcon'
 
-import { Appbar, TextInput } from 'react-native-paper'
+import {
+  Appbar,
+  Paragraph,
+  Subheading,
+  Switch,
+  TextInput,
+} from 'react-native-paper'
+
+interface CreateActivityState {
+  privateActivity: boolean
+}
 
 interface MyActivitiesProps {
   navigation: NavigationScreenProp<{}, {}>
 }
 
-export class MyActivities extends React.Component<MyActivitiesProps> {
+export class MyActivities extends React.Component<
+  MyActivitiesProps,
+  CreateActivityState
+> {
   public static navigationOptions: NavigationBottomTabScreenOptions = {
     title: 'My Activities',
     tabBarIcon: tabBarIcon('list'),
+  }
+
+  constructor(props: MyActivitiesProps) {
+    super(props)
+    this.state = {
+      privateActivity: true,
+    }
   }
 
   public render(): React.ReactNode {
@@ -46,6 +66,29 @@ export class MyActivities extends React.Component<MyActivitiesProps> {
             value={undefined}
             multiline={true}
           />
+          <View style={styles.inputContainerStyle}>
+            <View style={styles.row}>
+              <Subheading>
+                My activity is{' '}
+                {this.state.privateActivity ? 'private' : 'public'}
+              </Subheading>
+              <Switch
+                value={!this.state.privateActivity}
+                onValueChange={() =>
+                  this.setState(({ privateActivity }) => {
+                    return {
+                      privateActivity: !privateActivity,
+                    }
+                  })
+                }
+              />
+            </View>
+            <Paragraph>
+              {this.state.privateActivity
+                ? 'To join private activity, one need to request or invite.\nSensitive activity information is protected.'
+                : 'Public activity can be joined by anyone.\nAll information is public visible.'}
+            </Paragraph>
+          </View>
         </ScrollView>
       </View>
     )
@@ -62,5 +105,10 @@ const styles = StyleSheet.create({
   },
   inputContainerStyle: {
     margin: 8,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
 })
